7b07b2bce27951c6328d7f47cd58daad09ca3d06
千夏茹初 <chibayuki@foxmail.com>
2018-09-24 17:01:06 +0800

    Add files via upload

---
+++ WinFormApp/MainForm.cs
@@ -0,0 +1,71 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+Copyright © 2018 chibayuki@foxmail.com
+
+Windows窗体应用程序模板 (WinFormAppTemplate)
+Version 18.9.24.1700
+
+This file is part of "Windows窗体应用程序模板" (WinFormAppTemplate)
+
+"Windows窗体应用程序模板" (WinFormAppTemplate) is released under the GPLv3 license
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp
+{
+    public partial class MainForm : Form
+    {
+        private Com.WinForm.FormManager Me;
+
+        public Com.WinForm.FormManager FormManager
+        {
+            get
+            {
+                return Me;
+            }
+        }
+
+        private void _Ctor(Com.WinForm.FormManager owner)
+        {
+            InitializeComponent();
+
+            //
+
+            if (owner != null)
+            {
+                Me = new Com.WinForm.FormManager(this, owner);
+            }
+            else
+            {
+                Me = new Com.WinForm.FormManager(this);
+            }
+
+            //
+
+            FormDefine();
+        }
+
+        public MainForm()
+        {
+            _Ctor(null);
+        }
+
+        public MainForm(Com.WinForm.FormManager owner)
+        {
+            _Ctor(owner);
+        }
+
+        private void FormDefine()
+        {
+
+        }
+    }
+}
